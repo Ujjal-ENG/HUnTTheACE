29a2a437d19f2acb0cb50cdbcbf5fd4069ef9168
user <ujjalroy7862@gmail.com>
2022-12-28 17:47:18 +0600

    dom manupulation its little tricky

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cacrdObjectDefinations = [
   { id: 4, imagePath: "./images/card-AceSpades.png" },
 ];
 
+const cardBackImgPath = "./images/card-back-blue.png";
+
 const createCard = (cardItem) => {
   //create div element that make up a card
   const cardEl = document.createElement("div");
@@ -16,6 +18,29 @@ const createCard = (cardItem) => {
 
   const cardFrontImg = createElement("img");
   const cardBackImg = createElement("img");
+
+  //add class and id to card element
+  addClassToElement(cardEl, "card");
+  addIdToElement(cardEl, cardItem.id);
+
+  //add class to inner card element
+  addClassToElement(cardInnerEl, "card-inner");
+
+  //add class to front card element
+  addClassToElement(cardFrontEl, "card-front");
+
+  //add class to back card element
+  addClassToElement(cardBackEl, "card-back");
+
+  //add src attribute and appropriate value to img element
+  addSrcToImageEl(cardBackEl, cardBackImgPath);
+
+  //add src attribute
+  addSrcToImageEl(cardFrontEl, cardItem.imagePath);
+
+  //assign class to back image element of back of card
+  addClassToElement(cardBackEl, "card-img");
+  addClassToElement(cardFrontEl, "card-img");
 };
 
 const createElement = (elType) => {
@@ -25,3 +50,17 @@ const createElement = (elType) => {
 const addClassToElement = (el, className) => {
   el.classList.add(className);
 };
+
+const addIdToElement = (el, id) => {
+  el.id = id;
+};
+
+const addSrcToImageEl = (imgEl, src) => {
+  imgEl.src = src;
+};
+
+const addChildElement = (parentEl, childEl) => {
+  parentEl.appendChild(childEl);
+};
+
+
